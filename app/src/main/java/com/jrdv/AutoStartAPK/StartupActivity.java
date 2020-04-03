1c5747f8c6f0948122221b02f621ce45194c2254
joseramondelgado <interamaster@gmail.com>
2020-04-03 11:46:30 +0200

    //v04 menu elgir apk listo,,falta eñgirlo

--- app/src/main/java/com/jrdv/AutoStartAPK/StartupActivity.java
+++ app/src/main/java/com/jrdv/AutoStartAPK/StartupActivity.java
@@ -1,12 +1,21 @@
 package com.jrdv.AutoStartAPK;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static android.content.ContentValues.TAG;
 
@@ -14,6 +23,7 @@ import static android.content.ContentValues.TAG;
 //v0.1 compilado  para android studio ok de github:https://github.com/mienaikoe/DeltaMonitor
 //v02 ya enciendo pantalla e inicia hangout
 //v03 funciona timer ok pero el wlrelase no lo hace bien..
+//v04 menu elgir apk listo,,falta eñgirlo
 
 
 
@@ -23,6 +33,12 @@ public class StartupActivity extends Activity {
     public static final String MY_PREFS_NAME = "MyPrefsFile";
     private static final String TAG = "MotionDetectionActivity";
 
+
+    private PackageManager packageManager = null;
+    private List<ApplicationInfo> applist = null;
+    private ApplicationAdapter listadaptor = null;
+    private ListView listView;
+
     /**
      * {@inheritDoc}
      * @param savedInstanceState
@@ -30,6 +46,13 @@ public class StartupActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.chooseapkactivity);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+        listView = (ListView) findViewById(R.id.list);
+        packageManager = getPackageManager();
+        new LoadApplications().execute();
 
 
 
@@ -59,6 +82,8 @@ public class StartupActivity extends Activity {
 
 
 
+           // finish();
+
         }
 
 
@@ -77,8 +102,51 @@ public class StartupActivity extends Activity {
 
         }
     }
-    
-    
+
+
+
+    class LoadApplications extends AsyncTask<Void, Void, Void> {
+
+        private ProgressDialog progress = null;
+
+        @Override
+        protected void onPreExecute() {
+            progress = ProgressDialog.show(StartupActivity.this, null, "Loading applications...");
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            applist = checkForLaunchIntent(packageManager.getInstalledApplications(PackageManager.GET_META_DATA));
+            listadaptor = new ApplicationAdapter(StartupActivity.this,
+                    R.layout.row_item, applist);
+
+            return null;
+        }
+
+        private List<ApplicationInfo> checkForLaunchIntent(List<ApplicationInfo> list) {
+            ArrayList<ApplicationInfo> applist = new ArrayList<ApplicationInfo>();
+            for (ApplicationInfo info : list) {
+                try {
+                    if (null != packageManager.getLaunchIntentForPackage(info.packageName)) {
+                        applist.add(info);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return applist;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            listView.setAdapter(listadaptor);
+            progress.dismiss();
+            super.onPostExecute(aVoid);
+        }
+    }
    
         
 }
